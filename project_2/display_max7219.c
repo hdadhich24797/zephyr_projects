8c492e3cedb00e209a097628e4d1550dccf9404b
hdadhich24797 <hrishi24797@gmail.com>
2022-04-19 20:50:00 -0700

    Add driver's implemenation and update README

--- project_2/display_max7219.c
+++ project_2/display_max7219.c
@@ -1,23 +1,10 @@
-/*
- * Copyright (c) 2017 Jan Van Winkel <jan.van_winkel@dxplore.eu>
- * Copyright (c) 2019 Nordic Semiconductor ASA
- * Copyright (c) 2019 Marc Reilly
- * Copyright (c) 2019 PHYTEC Messtechnik GmbH
- * Copyright (c) 2020 Endian Technologies AB
- * Copyright (c) 2020 Kim Bøndergaard <kim@fam-boendergaard.dk>
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#define DT_DRV_COMPAT maxim,max7219
-
-//#include "display_max7219.h"
+#define DT_DRV_COMPAT maxim_max7219
 
 #include <device.h>
 #include <drivers/spi.h>
+#include <drivers/display.h>
 #include <drivers/gpio.h>
 #include <sys/byteorder.h>
-#include <drivers/display.h>
 #include <zephyr.h>
 #include <devicetree.h>
 #include <sys/util.h>
@@ -29,63 +16,98 @@
 #include <fsl_iomuxc.h>
 #include <drivers/pwm.h>
 #include <devicetree/pwms.h>
-//#include <soc.h>
-#include <dt-bindings/spi/spi.h>
 #include <drivers/spi.h>
 #include <string.h>
 
+/*  Config struct for max7219   */
 struct max7219_config {
 	const char *spi_name;
 	struct spi_config spi_config;
 	uint16_t height;
 	uint16_t width;
 };
-
+/*  Data struct for max7219    */
 struct max7219_data {
 	const struct max7219_config *config;
 	const struct device *spi_dev;
 };
 
-static int display_blanking_on()
-{
-    return 0;
-}
-
-static int display_blanking_off()
-{
-    return 0;
-}
-
-static int display_write(const struct device *dev,
-			 uint16_t length,
-			 const void *buf)
+/*  Init max7219    */
+static int max7219_init(const struct device *dev)
 {
 	struct max7219_data *data = (struct max7219_data *)dev->data;
-	struct spi_buf tx_buf;
-	struct spi_buf_set tx_bufs;
-    
-	tx_bufs.buffers = &tx_buf;
-	tx_bufs.count = 1;
+	struct max7219_config *config = (struct max7219_config *)dev->config;
 
-    for (int i = 0; i < length; i++)
-    {
-		ret = spi_write(data->spi_dev, &data->config->spi_config, &tx_bufs);
-		if (ret < 0) {
-			return ret;
-		}
-    }
-}
-
-
-static const struct display_driver_api max7219_api = {
-	.blanking_on = display_blanking_on,
-	.blanking_off = display_blanking_off,
-	.write = display_write,
-};
-static int max7219_pm_control()
-{
+	data->spi_dev = device_get_binding(config->spi_name);
+	if (data->spi_dev == NULL) {
+		return -1;
+	}
     return 0;
 }
+
+/*  Max7219 blinking on function    */
+static int display_blanking_on_my(const struct device *dev)
+{
+    struct max7219_data *data = (struct max7219_data *)dev->data;
+    int cmd = 0x0c00;
+	struct spi_buf tx_buf;
+	struct spi_buf_set tx_bufs;
+    tx_buf.buf = &cmd;
+    tx_buf.len = 2;
+	tx_bufs.buffers = &tx_buf;
+	tx_bufs.count = 1;
+    return spi_write(data->spi_dev, &data->config->spi_config, &tx_bufs);
+}
+
+/*  Max7219 blinking off function    */
+static int display_blanking_off_my(const struct device *dev)
+{
+    struct max7219_data *data = (struct max7219_data *)dev->data;
+    int cmd = 0x0c01;
+	struct spi_buf tx_buf;
+	struct spi_buf_set tx_bufs;
+    tx_buf.buf = &cmd;
+    tx_buf.len = 2;
+	tx_bufs.buffers = &tx_buf;
+	tx_bufs.count = 1;
+    return spi_write(data->spi_dev, &data->config->spi_config, &tx_bufs);
+}
+
+/*  Max7219 display function    */
+static int display_write_my(const struct device *dev,
+			 const uint16_t x,
+			 const uint16_t y,
+			 const struct display_buffer_descriptor *desc,
+			 const void *buf)
+{
+    printk("Inside display\n");
+
+    int ret = 0;
+	uint16_t *arr = (uint16_t *)buf;
+    int len = y;
+
+    struct max7219_data *data = (struct max7219_data *)dev->data;
+	struct spi_buf tx_buf;
+	struct spi_buf_set tx_bufs;
+	tx_bufs.buffers = &tx_buf;
+	tx_bufs.count = 1;
+    for (int i = 0; i < len; i++)
+    {
+        printk("%x\r\n", arr[i]);
+
+        tx_buf.buf = &arr[i];
+        tx_buf.len = 2;
+        ret = spi_write(data->spi_dev, &data->config->spi_config, &tx_bufs);
+    }
+    return ret;
+}
+
+/*  APIS for MAX7219    */
+static const struct display_driver_api max7219_api = {
+	.blanking_on = display_blanking_on_my,
+	.blanking_off = display_blanking_off_my,
+	.write = display_write_my,
+};
 
 #define MAX7219_INIT(inst)							\
 	static struct max7219_data max7219_data_ ## inst;			\
@@ -96,7 +118,7 @@ static int max7219_pm_control()
 		.spi_config.frequency = UTIL_AND(				\
 			DT_HAS_PROP(inst, spi_max_frequency),			\
 			DT_INST_PROP(inst, spi_max_frequency)),			\
-		.spi_config.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(8),	\
+		.spi_config.operation = SPI_OP_MODE_MASTER | SPI_WORD_SET(16) | SPI_MODE_CPOL | SPI_TRANSFER_MSB,	\
 		.spi_config.cs = UTIL_AND(					\
 			DT_INST_SPI_DEV_HAS_CS_GPIOS(inst),			\
 			&(max7219_data_ ## inst.cs_ctrl)),			\
@@ -107,7 +129,7 @@ static int max7219_pm_control()
 	static struct max7219_data max7219_data_ ## inst = {			\
 		.config = &max7219_config_ ## inst,				\
 	};									\
-	DEVICE_DT_INST_DEFINE(inst, max7219_init, max7219_pm_control,		\
+	DEVICE_DT_INST_DEFINE(inst, max7219_init, NULL,		\
 			      &max7219_data_ ## inst, &max7219_config_ ## inst,	\
 			      APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY,	\
 			      &max7219_api);
--- project_2/readme.txt
+++ project_2/readme.txt
@@ -5,10 +5,6 @@ p2 rgb <x1> <x2> <x2>
 p2 ledm <row> <val1> <val2> ...
 p2 ledb <0 or 1>
 
-NOTE: I am able to implement above all in the main.c code but driver implementation is pending.
-I have tried to implement the driver code but due to lack of time I was not able to complete it.
-
-I was able to execute the MAX7218 Display functionality in the main code by using hardcoded values.
 
 To build the code:
 
@@ -18,3 +14,5 @@ To flash:
 west flash
 
 I have written test_rgb_pwm for your refernece at the last line of the code.
+
+I am using my driver's API for display driver API calls.
